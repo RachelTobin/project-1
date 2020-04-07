22c712dcd41cdd4a99acd8c05460602164a6898c
Rachel Tobin <rtobin@uoregon.edu>
2020-04-07 14:11:24 -0700

    Clean up

--- javascripts/functions.js
+++ javascripts/functions.js
@@ -4,11 +4,9 @@ arrAvg = function(arrList) {
   for (index = 0; index < arrList.length; index++) {
     console.log(arrList[index]);
     total += arrList[index];
-
 }
   return total/arrList.length;
 };
-
 
 arrAvg([2, 3, 5, 7, 9]);
 
@@ -24,7 +22,6 @@ arrMax = function(arrList) {
     return max;
 };
 
-
 arrMax([6, 2, 4]);
 
 
@@ -38,6 +35,5 @@ sumEvens = function(arrList){
     }
   return sum;
 };
-
 
 console.log(sumEvens([1, 2, 6]));
