7e904b9d6c86c354bba64c05b03a8251e21d7d4e
Rachel Tobin <rtobin@uoregon.edu>
2020-04-10 12:31:41 -0700

    Final version of diceNamic series, added background images and adjusted style

--- p1/javascripts/diceNamic.js
+++ p1/javascripts/diceNamic.js
@@ -3,43 +3,46 @@
 //define helper functions here
 
 let rollDie = () => {
-    return Math.floor (Math.random() * 6) + 1;
+  return Math.floor(Math.random() * 6) + 1;
 };
 
 let controller = () => {
   let target, die1, die2, rollCount, die1URL, die2URL, imgELem1, imgElem2, cheeryMsg;
 
+
   target = document.querySelector('input').value;
   console.log(target);
+  // to ensure target is a possible outcome
+  if (target >= 2 && target <= 12) {
 
+    // roll target number (and connt the rolls
+    rollCount = 0;
+    do {
 
-// roll target number (and connt the rolls
-rollCount = 0;
-do{
+      //roll the dice
+      //die1 = rollDie();
+      die1 = rollDie();
+      die2 = rollDie();
+      // increment the roll counter
 
-  //roll the dice
-  //die1 = rollDie();
-  die1 = rollDie();
-  die2 = rollDie();
-  // increment the roll counter
+      rollCount += 1;
+      // log dice and counter
+      console.log(`${die1} ${die2} ${rollCount}`);
+    } while (die1 + die2 != target);
 
-  rollCount +=1;
-  // log dice and counter
-  console.log(`${die1} ${die2} ${rollCount}`);
-} while (die1 + die2 != target);
+    // update images on web page
+    die1URL = `images/die${die1}.gif`;
+    imgElem1 = document.querySelector("img");
+    imgElem1.setAttribute("src", die1URL);
 
-// update images on web page
-die1URL = `images/die${die1}.gif`;
-imgElem1 = document.querySelector("img");
-imgElem1.setAttribute("src", die1URL);
+    die2URL = `images/die${die2}.gif`;
+    imgElem2 = document.querySelectorAll("img")[1];
+    imgElem2.setAttribute("src", die2URL);
 
-die2URL = `images/die${die2}.gif`;
-imgElem2 = document.querySelectorAll("img")[1];
-imgElem2.setAttribute("src", die2URL);
-
-// display rolls
-cheeryMsg = `You hit your number in ${rollCount} roll${(rollCount == 1)?"":"s"}! `;
-document.querySelector("div").innerHTML = cheeryMsg;
+    // display rolls
+    cheeryMsg = `You hit your number in ${rollCount} roll${(rollCount == 1)?"":"s"}! `;
+    document.querySelector("div").innerHTML = cheeryMsg;
+  }
 
 };
 //Register the controller after the DOM is complete
